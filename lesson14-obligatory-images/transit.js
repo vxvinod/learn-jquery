e6743fb64783e21b9d732deaa3aa5fee72a86721
vxvinod <vinodkmrmurugan@gmail.com>
2013-10-07 08:09:11 +0530

    obligatory images is done wowwwwwww

--- lesson14-obligatory-images/transit.js
+++ lesson14-obligatory-images/transit.js
@@ -1,23 +1,49 @@
 (function($){
 
-	var sliderUL=$('div.slider').css('overflow','hidden').children('ul'),
+	var sliderUL= $('div.slider').css('overflow','hidden').children('ul'),
 	imgs=sliderUL.find('img'),
 	imgWidth=imgs[0].width,
 	imgsLen=imgs.length,
 	current=1;
+	totalImgWidth= imgsLen*imgWidth;
 	$('div#slider-nav').show().find('button').on('click',function(){
 		var direction=$(this).data('dir');
-		console.log(direction);
+		//console.log(direction);
+		loc=imgWidth;
 
 		(direction=='next')? ++current:--current
-		console.log(current);
+		//console.log(current);
+	
+
+	if(current===0)
+	{
+		current=imgsLen;
+		loc=totalImgWidth-imgWidth;
+		direction='next';
+	} 
+	 else if(current - 1 == imgsLen) 
+	{
+		current=1;
+		loc=0;
+	}
+
+	transition(sliderUL,loc,direction);
 	});
 
-	if(current===0){
-		current=imgsLen;
-		direction='next';
-	}else if{
-		current=1;
+	function transition(container,loc,direction)
+	{
+		var unit;
+
+		if(direction && loc!=0) {
+			unit = (direction == 'next') ? '-=' : '+=';
+		}
+
+		console.log("clicked");
+		container.animate({
+			'margin-left': unit ? (unit + loc) : loc
+			
+		});
 	}
+
 
 })(jQuery);
