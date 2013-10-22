370c798ca88a97264485f40389f97e90e4c8cf90
vxvinod <vinodkmrmurugan@gmail.com>
2013-10-22 08:42:51 +0530

    protype is completed

--- lesson15-protype-and-inheritance/transit.js
+++ lesson15-protype-and-inheritance/transit.js
@@ -1,18 +1,26 @@
 function Slider(container,nav){
 	this.container=container;
-	this.nav=nav;
+	this.nav=nav.show();
 	this.imgs=this.container.find('img');
 	this.imgsWidth=this.imgs[0].width;
 	this.imgsLen=this.imgs.length;
 	this.current=0;
 }
 
-Slider.protoype.transition = function(coords){
+Slider.prototype.transition = function( coords ) {
 	this.container.animate({
-
-	'margin-left': coords||-(this.current * this.imgsWidth)
+	'margin-left': coords || -( this.current * this.imgsWidth )
 	});
-}
+
+};
+	Slider.prototype.setCurrent = function( dir ) {
+
+		var pos = this.current;
+		pos+= (~~(dir === 'next') || -1) ;
+		this.current = (pos<0)?  this.imgsLen-1:pos% this.imgsLen;
+		return pos;
+	};
+
+
 
 	
-
